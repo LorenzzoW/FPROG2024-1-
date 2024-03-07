d3b1efe7bdd69b6d8d19779d4e6f8502a317b7c3
LorenzzoW <lorenzzowilliam@gmail.com>
2024-03-06 21:40:59 -0300

    Create primeiro exemplo.py

---
+++ primeiros exemplos/primeiro exemplo.py
@@ -0,0 +1,23 @@
+# Comando de saída
+print('Hello!')
+
+# Criando uma variavel numérica e atribuindo um valor
+num= 10
+
+# Imprimindo um valor da variavel num
+print(num)
+
+#Trocando o valor da variavel num
+num = -1
+
+print(num)
+
+# Leitura (comando de entrada)
+nome = input('Digite seu nome e pressione ENTER:')
+print(nome)
+
+
+# Leitura de valor inteiro para a variável idade
+idade = input('Digite a sua idade: ')
+
+print('seu nome é ',nome, ' e sua idade é', idade)
