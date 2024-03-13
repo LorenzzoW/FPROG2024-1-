9c6dcab71bff050817a264899176f3965d0ab7ca
LorenzzoW <lorenzzowilliam@gmail.com>
2024-03-13 20:14:40 -0300

    Create Ex3.py

---
+++ primeiros exemplos/Ex3.py
@@ -0,0 +1,9 @@
+# Exibir a mensagem em questão
+print("Qual o verdadeiro nome super-heroi Batman?")
+print("a) Bruce Wayne")
+print("b) Clark Kent")
+print("c) Peter Parker")
+print("d) Peter Parker")
+print("e) Steve Rogers")
+nome = input ("Digite a alternativa correta:")
+print("A sua resposta foi" , nome +", A resposta correta é A")
